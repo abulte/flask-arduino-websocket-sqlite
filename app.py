1495549cda4d6072490e5981c11d4694c16adc42
Alexandre Bulte <alexandre@bulte.net>
2013-03-05 14:29:49 +0100

    last try with gevent-websocket

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+#
 # Copyright 2013 Alexandre Bulté <alexandre[at]bulte[dot]net>
 # 
 # This Source Code Form is subject to the terms of the Mozilla Public
@@ -11,7 +11,13 @@ from geventwebsocket.handler import WebSocketHandler
 from gevent.pywsgi import WSGIServer
 from flask import Flask, request, render_template, jsonify
 
-import datetime, json
+import datetime
+import json
+import serial
+import collections
+import time
+
+import gevent
 
 app = Flask(__name__)
 app.debug = True
@@ -35,7 +41,7 @@ def query_db_orig(query, args=(), one=False):
 
 def date_from_timestamp(ts):
     mydate = datetime.datetime.fromtimestamp(ts)
-    return mydate.strftime('%d/%m/%y - %H:%M')
+    return mydate.strftime('%d/%m/%y - %H:%M:%S')
 
 
 def query_db(dbtouse, query, args=(), convert_date=True):
@@ -64,24 +70,64 @@ def index():
 def history():
     return render_template('history.html')
 
+def connect_serial():
+    serialport = '/dev/ttyACM0'
+    baudrate = '9600'
+    ser = serial.Serial(serialport, baudrate)
+    ser.stopbits = 2
+    return ser
+
+def make_result(values, hum_list):
+    result = []
+    # date
+    result.append(date_from_timestamp(time.time()))
+    # temp, rhum, thum
+    result.extend(values[1::2])
+    # thum average
+    hum_list.append(float(values[5]))
+    result.append(int(float(sum(hum_list)) / len(hum_list)))
+    # send result through websocket
+    return result, hum_list
+
+def _read_serial(ws):
+    input = ''
+    hum_list = collections.deque(maxlen=10)
+    ser = connect_serial()
+    # todo: exit loop when websocket disconnects?
+    # todo: timeout?
+    while True:
+        input += ser.read()
+        # gevent.sleep(0)
+        while input.find("\r\n") != -1:
+            chopped_line = input[:input.find("\r\n")]
+            input = input[input.find("\r\n")+2:]
+            if chopped_line.find('temperature') != -1:
+                values = chopped_line.split(' ')
+                if len(values) > 5:
+                    result, hum_list = make_result(values, hum_list)
+                    ws.send(json.dumps({'latest': result}))
+                else:
+                    ws.send(json.dumps({'error':'wrong length of parsed line'}))
+            else:
+                ws.send(json.dumps({'error':'wrong line format'}))
+
+
+def read_serial(ws):
+    ws.send(json.dumps('coucou'))
+    i = 0
+    while i < 60:
+        ws.send(json.dumps(i))
+        time.sleep(1)
+        i += 1
+    ws.send(json.dumps('end'))
+
 # get the latest measure
+# directly through serial
 @app.route('/api/live')
 def api_live():
     if request.environ.get('wsgi.websocket'):
         ws = request.environ['wsgi.websocket']
-        readings = query_db(DATABASE, 'SELECT * FROM readings ORDER BY ts DESC limit 1', convert_date=False)
-        readings_salon = query_db(DATABASE_SALON, 'SELECT * FROM readings ORDER BY ts DESC limit 1', convert_date=False)
-        readings_tobacco = query_db(DATABASE_TOBACCO, 'SELECT * FROM readings ORDER BY ts DESC limit 1', convert_date=False)
-        latest = 0
-        for r in [readings[0], readings_tobacco[0], readings_salon[0]]:
-            if r[0] > latest:
-                result = r
-                latest = r[0]
-        print result
-        result[0] = date_from_timestamp(result[0])
-        if len(result) < 4:
-            result.append('na')
-        ws.send(json.dumps({'latest': result}))
+        read_serial(ws)
     return jsonify({'status': 'ok'})
 
 @app.route('/api')
